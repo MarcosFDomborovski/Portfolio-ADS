7ccb45eff29e3a33ef3a74af9a8f94fca09ffdb8
Marcos F. Domborovski <marcosfelipedomborovski@gmail.com>
2024-06-26 14:31:06 -0300

    Finalização da página Protocolos

---
+++ assets/menu/tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          customBlue: '#243444', // ou 'rgb(36, 52, 68)'
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  };
+  
